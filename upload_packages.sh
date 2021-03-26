7e65228573a2bebe68305a41640874b86472146f
Adrian Martin <adrian@kindred.ai>
2021-03-26 14:11:08 -0400

    fix circleci and arm builds

--- upload_packages.sh
+++ upload_packages.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# run via build_and_action.sh, $1 will be VERSION (unused), $2 will be DOCKER_FILE_SUFFIX (e.g. focal)
 set -euo pipefail
-echo "yes" | $HOME/.local/bin/kin_ci upload /build/*.deb
+echo "yes" | $HOME/.local/bin/kin_ci upload --deb-distro ubuntu --deb-version $2 /build/*.deb
