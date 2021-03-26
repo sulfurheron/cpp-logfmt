9a4e0f2d67ec84394811e06b3fff0ed2b6bf02ec
Adrian Martin <adrian@kindred.ai>
2021-03-26 15:18:59 -0400

    version string is incompatible for some reason

--- create_package.sh
+++ create_package.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# run via build_and_action.sh, $1 will be VERSION, $2 will be DOCKER_FILE_SUFFIX (e.g. focal)
 set -euo pipefail
 fpm \
     -t deb \
@@ -11,6 +10,6 @@ fpm \
     --depends libboost-thread-dev \
     --depends libboost-dev \
     --name kin_logfmt \
-    --version $1_$2 \
+    --version $1 \
     usr
 
